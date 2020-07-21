228d596e0ab6de9abdcda1388a377b3cd8b13e75
Ariel May <aemd199711@gmail.com>
2020-07-20 19:45:28 -0500

    Validacion de formulario de contacto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Form from "./components/Form";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  font-family: "Roboto", sans-serif;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  h1 {
+    grid-column-start: 1;
+    grid-column-end: -1;
+    text-align: center;
+    text-transform: uppercase;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppStyled className="App">
+      <h1>contact form</h1>
+      <Form />
+    </AppStyled>
+  );
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+
+  label {
+    padding-bottom: 5px;
+  }
+
+  input {
+    border: 1px solid black;
+    padding: 4px 12px;
+    &:focus {
+      outline-color: #80bdff;
+      box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+  }
+
+  textarea {
+    height: 100px;
+    &:focus {
+      outline-color: #80bdff;
+      box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+  }
+
+  button {
+    width: 100px;
+    height: 30px;
+    align-self: flex-end;
+    margin-top: 20px;
+  }
+`;
+
+const Form = (props) => {
+  const [contactData, setContactData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    description: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const { name, email, description, subject } = contactData;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (
+      name.trim() === "" ||
+      email.trim === "" ||
+      description.trim === "" ||
+      subject.trim() === ""
+    ) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+  };
+
+  const updateState = (e) => {
+    setContactData({
+      ...contactData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <FormStyled onSubmit={onSubmit}>
+      {error && <h1>All fields are required</h1>}
+      <div>
+        <label>Name</label>
+        <input
+          name="name"
+          onChange={updateState}
+          placeholder="Enter name"
+          value={name}
+        ></input>
+      </div>
+
+      <div>
+        <label>Email</label>
+        <input
+          name="email"
+          onChange={updateState}
+          placeholder="Enter Email"
+          value={email}
+        ></input>
+      </div>
+
+      <div>
+        <label>Subject</label>
+        <input
+          name="subject"
+          onChange={updateState}
+          placeholder="Enter Subject"
+          value={subject}
+        ></input>
+      </div>
+
+      <div>
+        <label>Description</label>
+        <textarea
+          name="description"
+          onChange={updateState}
+          placeholder="Enter Description"
+          value={description}
+        ></textarea>
+        <button>Send</button>
+      </div>
+    </FormStyled>
+  );
+};
+
+export default Form;
